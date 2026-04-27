6f3d78431cc5ac94d8fb1501139e1fbcfc565768
Daizen <jkcompanero@gmail.com>
2026-04-27 18:03:40 +0800

    This is the second commit which is about locating someone who has highest gwa among all students

--- greater_gwa_finder.py
+++ greater_gwa_finder.py
@@ -1,1 +1,19 @@
-print("Identifying the students that has a highest gwa")
+filename = r"C:\Python Programs\file_handling\GWA FINDER\student"
+
+file = open(filename, "r")
+
+best_gwa = 5.0
+best_student = ""
+
+for line in file:
+    data = line.strip().split(",")
+    name = data[0]
+    gwa = float(data[1])
+    
+    if gwa < best_gwa:
+        best_gwa = gwa
+        best_student = name
+
+file.close()
+
+print("Top Student:", best_student, "GWA:", best_gwa)
