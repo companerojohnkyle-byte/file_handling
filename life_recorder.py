6965778345a71489c0ae07ab980e8f02d5c75ce6
Daizen <jkcompanero@gmail.com>
2026-04-27 19:20:59 +0800

    This is the second draft for this program, the function is to create a file that squares and cube the 20 integer numbers

--- life_recorder.py
+++ life_recorder.py
@@ -1,17 +1,23 @@
-class MyLifeWriter:
-    def __init__(self):
-        self.filename = r"C:\Python Programs\file_handling\My Life txt\mylife.txt"
+integers = r"C:\Python Programs\file_handling\Squared and Cubed Processor\Integers.txt"
+odds = r"C:\Python Programs\file_handling\Squared and Cubed Processor\odd.txt"
+even = r"C:\Python Programs\file_handling\Squared and Cubed Processor\even.txt"
 
-    def write_to_file(self):
-        with open(self.filename, "w") as file:
-            more_lines = "y"
-            
-            while more_lines.lower() == "y":
-                line_content = input("Enter line: ")
-                file.write(line_content + "\n")               
-                more_lines = input("Are there more lines y/n? ")
-                
-        print("\nLines have been saved to mylife.txt successfully!")
+file_in = open(integers, "r")
 
-writer = MyLifeWriter()
-writer.write_to_file()
+file_even = open(even, "w")
+file_odd = open(odds, "w")
+
+for line in file_in:
+    number = int(line.strip())
+    
+    if number % 2 == 0:
+        result = number * number
+        file_even.write(str(result) + "\n")
+    else:
+        result = number * number * number
+        file_odd.write(str(result) + "\n")
+
+file_in.close()
+file_even.close()
+file_odd.close()
+print("Process complete: Files created.")
