8626fbcceec5db282094d3c58a5a0a4a0bf812df
Daizen <jkcompanero@gmail.com>
2026-04-27 20:24:49 +0800

    This is the second commit of this code, the function of this code is to square and cube the given numbers in the integers.

--- squared_cubed_processor.py
+++ squared_cubed_processor.py
@@ -1,1 +1,14 @@
-print("This is the first canvas for squared and cubed processor")
+file_in = open(r"C:\Python Programs\file_handling\Squared and Cubed Processor\Integers.txt", "r")
+file_squared = open(r"C:\Python Programs\file_handling\Squared and Cubed Processor\squared.txt", "w")
+file_cubed = open(r"C:\Python Programs\file_handling\Squared and Cubed Processor\cubed.txt", "w")
+
+for line in file_in:
+    n = int(line.strip())
+    if n % 2 == 0:
+        file_squared.write(str(n * n) + "\n")
+    else:
+        file_cubed.write(str(n * n * n) + "\n")
+
+file_in.close()
+file_squared.close()
+file_cubed.close()
