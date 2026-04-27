8587cbc946c58a449a4a3c8c20202ad490c1060a
Daizen <jkcompanero@gmail.com>
2026-04-27 19:40:38 +0800

    This is the second commit, I created some program that saves the line that you've entered in the txt.

--- mylife_recorder.py
+++ mylife_recorder.py
@@ -1,1 +1,15 @@
-print("This is the first initial for MY Life recorder")
+filename = r"C:\Python Programs\file_handling\My Life txt\mylife.txt"
+
+file = open(filename, "w")
+
+more_lines = "y"
+
+while more_lines == "y":
+    line_content = input("Enter line: ")
+    file.write(line_content + "\n")
+    
+    more_lines = input("Are there more lines y/n? ")
+
+file.close()
+
+print("Done writing to file.")
