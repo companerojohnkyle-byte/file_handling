0a7d1d0ca78ec48268c9fcc89c5416606b789491
Daizen <jkcompanero@gmail.com>
2026-04-27 20:10:05 +0800

    This is the final commit for this program. The function of this code is to save the information that was written in line to your file/txt after the interaction.

--- mylife_recorder.py
+++ mylife_recorder.py
@@ -1,15 +1,19 @@
-filename = r"C:\Python Programs\file_handling\My Life txt\mylife.txt"
+class MyLifeRecorder:
+    def __init__(self):
+        self.filename = r"C:\Python Programs\file_handling\My Life txt\mylife.txt"
 
-file = open(filename, "w")
+    def write_to_file(self):
+        with open(self.filename, "w") as file:
+            more_lines = "y"
+            
+            while more_lines.lower() == "y":
+                line_content = input("Enter line: ")
+                
+                file.write(line_content + "\n")
+                
+                more_lines = input("Are there more lines y/n? ")
+                
+        print("\nLines have been saved to mylife.txt successfully!")
 
-more_lines = "y"
-
-while more_lines == "y":
-    line_content = input("Enter line: ")
-    file.write(line_content + "\n")
-    
-    more_lines = input("Are there more lines y/n? ")
-
-file.close()
-
-print("Done writing to file.")
+writer = MyLifeRecorder()
+writer.write_to_file()
